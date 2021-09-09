3f13be47bcd1fbbf13e96448083879f65c590c40
rain <rain@pop-os.localdomain>
2021-09-09 19:04:52 +0700

    added tenaga kerja to kegiatan

--- src/entities/KegiatanEntity.ts
+++ src/entities/KegiatanEntity.ts
@@ -2,40 +2,48 @@ import { DateTime } from "luxon";
 import { Expose, Transform, TransformFnParams, Type } from "class-transformer";
 import "reflect-metadata";
 import { IsLuxonDateTime } from "../commons/validations/IsLuxonDateTime";
+import TenagaKerjaEntity, { TenagaKerjaEntities } from "./TenagaKerjaEntity";
 
 class KegiatanEntity {
   KegiatanId?: number;
 
-  @IsLuxonDateTime()
-  @Type(() => Date)
+  @Type(() => DateTime)
   @Transform((data: TransformFnParams) => DateTime.fromJSDate(data.value), {
     toClassOnly: true,
   })
+  @Expose()
   TanggalWaktuAwal: DateTime;
 
-  @IsLuxonDateTime()
-  @Type(() => Date)
+  @Type(() => DateTime)
   @Transform((data: TransformFnParams) => DateTime.fromJSDate(data.value), {
     toClassOnly: true,
   })
+  @Expose()
   TanggalWaktuAkhir: DateTime;
 
   @Expose() Uraian: string;
   @Expose() Lokasi: string;
   @Expose() Keterangan: string;
+  @Type(() => TenagaKerjaEntity)
+  @Expose()
+  TenagaKerjas: TenagaKerjaEntities;
 
   constructor(
+    KegiatanId: number,
     TanggalWaktuAwal: DateTime,
     TanggalWaktuAkhir: DateTime,
     Uraian: string,
     Lokasi: string,
-    Keterangan: string
+    Keterangan: string,
+    TenagaKerjas: TenagaKerjaEntities
   ) {
+    this.KegiatanId = KegiatanId;
     this.TanggalWaktuAwal = TanggalWaktuAwal;
     this.TanggalWaktuAkhir = TanggalWaktuAkhir;
     this.Uraian = Uraian;
     this.Lokasi = Lokasi;
     this.Keterangan = Keterangan;
+    this.TenagaKerjas = TenagaKerjas;
   }
 }
 
--- src/pdf/KegiatanHarianPDF.ts
+++ src/pdf/KegiatanHarianPDF.ts
@@ -1,11 +1,12 @@
 import PDFDocument from "pdfkit";
-import {DateTime} from 'luxon';
+import { DateTime } from "luxon";
 import KegiatanEntity from "../entities/KegiatanEntity";
+import TenagaKerjaEntity from "../entities/TenagaKerjaEntity";
 
 const KegiatanHarianPDF = (
   onData: (...args: any[]) => void,
   onEnd: (...args: any[]) => void,
-  kegiatans:  KegiatanEntity[]
+  kegiatan: KegiatanEntity
 ) => {
   const doc = new PDFDocument({ bufferPages: true });
   doc.on("data", onData);
@@ -14,24 +15,25 @@ const KegiatanHarianPDF = (
     .font("Times-Bold")
     .fontSize(10)
     .text("LAPORAN KEGIATAN HARIAN", { align: "center" });
-  kegiatans.forEach((kegiatan)=>{
-    doc
-      .font("Times-Roman")
-      .fontSize(10)
-      .text(`Uraian : ${kegiatan.Uraian}`);
-    doc
-      .font("Times-Roman")
-      .fontSize(10)
-      .text(`Lokasi : ${kegiatan.Lokasi}`);
-    doc
-      .font("Times-Roman")
-      .fontSize(10)
-      .text(`Waktu: ${kegiatan.TanggalWaktuAwal.setLocale('id').toLocaleString(DateTime.DATETIME_FULL)} s.d. ${kegiatan.TanggalWaktuAkhir.setLocale('id').toLocaleString(DateTime.DATETIME_FULL)}`)
-    doc
-      .font("Times-Roman")
-      .fontSize(10)
-      .text(`Keterangan : ${kegiatan.Keterangan}`);
-  })
+  doc.font("Times-Roman").fontSize(10).text(`Uraian : ${kegiatan.Uraian}`);
+  doc.font("Times-Roman").fontSize(10).text(`Lokasi : ${kegiatan.Lokasi}`);
+  doc
+    .font("Times-Roman")
+    .fontSize(10)
+    .text(
+      `Waktu: ${kegiatan.TanggalWaktuAwal.setLocale("id").toLocaleString(
+        DateTime.DATETIME_FULL
+      )} s.d. ${kegiatan.TanggalWaktuAkhir.setLocale("id").toLocaleString(
+        DateTime.DATETIME_FULL
+      )}`
+    );
+  doc
+    .font("Times-Roman")
+    .fontSize(10)
+    .text(`Keterangan : ${kegiatan.Keterangan}`);
+  kegiatan.TenagaKerjas.forEach((tk: TenagaKerjaEntity) => {
+    doc.font("Times-Roman").fontSize(10).text(`${tk.Jenis}: ${tk.Jumlah}`);
+  });
   doc.end();
 };
 
